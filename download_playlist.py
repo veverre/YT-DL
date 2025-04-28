15775ca7132ba78ab46a38c9b4adbf00650be9db
Veverre <hrv.leroy@gmail.com>
2025-04-28 13:48:28 +0200

    user can select between 2 formats

--- download_playlist.py
+++ download_playlist.py
@@ -1,16 +1,22 @@
 import yt_dlp
 import os
+import inquirer
 
 # Fonction pour télécharger l'audio d'une vidéo
-def download_audio(url, download_path):
+def download_audio(url, download_path, format, quality):
     # Configuration des options de téléchargement
+
+    postprocessor = {
+        'key': 'FFmpegExtractAudio',  # Nom du post-processeur correct
+        'preferredcodec': format,  # Le format désiré
+    }
+
+    if quality:
+        postprocessor['preferredquality'] = quality
+
     ydl_opts = {
         'format': 'bestaudio/best',
-        'postprocessors': [{         # Convertir l'audio en MP3 après le téléchargement
-            'key': 'FFmpegExtractAudio',  # Nom du post-processeur correct
-            'preferredcodec': 'mp3',  # Format MP3
-            'preferredquality': '192',  # Qualité audio (192kbps)
-        }],
+        'postprocessors': [postprocessor],
         'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'),  # Nom du fichier de sortie (titre de la vidéo)
         'quiet': False,  # Affiche les logs (si True, tout sera silencieux)
     }
@@ -33,13 +39,13 @@ def download_audio(url, download_path):
             print(f"Erreur pour la vidéo {url}: {e}, vidéo ignorée.")
 
 # Fonction pour télécharger une playlist YouTube
-def download_playlist(playlist_url, download_path):
+def download_playlist(playlist_url, download_path, format, quality):
     # Vérifie si le dossier de destination existe, sinon le crée
     if not os.path.exists(download_path):
         os.makedirs(download_path)  # Créer le dossier si nécessaire
 
     # Lance le téléchargement de la playlist
-    download_audio(playlist_url, download_path)
+    download_audio(playlist_url, download_path, format, quality)
 
 # Point d'entrée du script
 if __name__ == "__main__":
@@ -48,6 +54,35 @@ if __name__ == "__main__":
     
     # Demander à l'utilisateur le chemin où sauvegarder les fichiers audio
     download_path = input("Enter the folder to save the audio files: ")
+
+    formatList = [
+        inquirer.List('format',
+            message="Quel format audio voulez-vous?",
+            choices=[
+                'mp3',
+                'wav',
+            ],
+        ),
+    ]
+
+    format = inquirer.prompt(formatList)['format']
+
+    qualityList = [
+        inquirer.List('quality',
+            message="Quelle qualité audio voulez-vous?",
+            choices=[
+                '128k',
+                '192k',
+                '256k',
+                '320k',
+            ],
+        ),
+    ]
+
+    if format == 'mp3':
+        quality = inquirer.prompt(qualityList)['quality']
+    else:
+        quality = None
     
     # Télécharger la playlist
-    download_playlist(playlist_url, download_path)
+    download_playlist(playlist_url, download_path, format, quality)
